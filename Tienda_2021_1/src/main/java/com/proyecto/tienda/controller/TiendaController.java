bc264ada8c63b0277fcec3c7df1635935cdbbf62
jonatancb2020 <i201822610@cibertec.edu.pe>
2021-05-11 16:10:17 -0500

    add Dependencias faltantes

---
+++ Tienda_2021_1/src/main/java/com/proyecto/tienda/controller/TiendaController.java
@@ -0,0 +1,13 @@
+package com.proyecto.tienda.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TiendaController {
+	
+	@RequestMapping("/index")
+	public String ver() {
+		return "index";
+	}
+}
