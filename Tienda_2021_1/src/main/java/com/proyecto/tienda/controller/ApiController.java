bf2eb54f1623479e7f5648862418a299883229d1
CarlosCotrina10 <carlos.cotrina95@gmail.com>
2021-06-05 17:14:44 -0500

    Api para el chatbot

---
+++ Tienda_2021_1/src/main/java/com/proyecto/tienda/controller/ApiController.java
@@ -0,0 +1,48 @@
+package com.proyecto.tienda.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import com.proyecto.tienda.entidad.Boleta;
+import com.proyecto.tienda.entidad.BoletaJSON;
+import com.proyecto.tienda.servicio.ApiServicio;
+
+@RestController
+@RequestMapping("/rest/boleta")
+public class ApiController {
+	
+	@Autowired
+	private ApiServicio servicio;
+
+	@PutMapping
+	public ResponseEntity<Boleta> actualiza(@RequestBody BoletaJSON bol) {
+
+		System.out.println("API REST >>>>> " + bol.getNumBoleta());
+		Optional<Boleta> optBoleta = servicio.obtienePorNum(bol.getNumBoleta());
+		
+		if (optBoleta.isPresent()) {
+			Boleta boleta = optBoleta.get();
+			boleta.setEstado(0);
+			boleta = servicio.insertaActualiza(boleta);
+			if (boleta != null) {
+				return ResponseEntity.ok(boleta);
+			} else {
+				System.out.println("error");
+				return ResponseEntity.badRequest().build();	
+			}
+		} else {
+			System.out.println("no hay");
+			return ResponseEntity.badRequest().build();
+		}
+
+	}
+
+
+}
